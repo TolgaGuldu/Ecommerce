7c390e31172ae9e551d1ea32604ab2cfd41ef969
Tolga Güldütuna <33281710+TolgaGuldu@users.noreply.github.com>
2022-10-26 17:17:28 +0300

    son

---
+++ install documents/start.bat
@@ -0,0 +1,3 @@
+@echo off
+start java -jar ..\Ecommerce\om\build\libs\om-1.jar
+start java -jar ..\Ecommerce\pim\build\libs\pim-1.jar
--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start java -jar ..\Ecommerce\om\build\libs\om-1.jar
-start java -jar ..\Ecommerce\pim\build\libs\pim-1.jar
